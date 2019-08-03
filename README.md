351e33c8d0e8d0b438cfbde11929d0e0e19710a9
ankitjain28may <ankitjain28may77@gmail.com>
2019-08-04 04:43:02 +0530

    Added drupal-installer

---
+++ README.md
@@ -0,0 +1,27 @@
+# DrupalHost
+
+DrupalHost is a Drupal Installer plugin similar to the `laravel/installer` plugin.
+
+## Installation
+
+1. First, download the Drupal installer using Composer:
+
+    ```shell
+        composer global require ankitjain28may/drupalhost
+    ```
+
+2. Once it is installed, the `drupalhost` new command will create a fresh Drupal installation in the directory you specify.
+
+    ```shell
+        drupalhost new blog
+    ```
+
+**Note:** It takes the latest development branch i.e `8.x` of the [drupal-composer](https://github.com/drupal-composer/drupal-project) project.
+
+ ## Contribute
+
+>Feel free to contribute
+
+## License
+
+>Copyright (c) 2019 Ankit Jain - Released under the MIT License
---
+++ src/NewCommand.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace Ankitjain28may\DrupalHost\Console;
+
+use ZipArchive;
+use RuntimeException;
+use GuzzleHttp\Client;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class NewCommand extends Command
+{
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('new')
+            ->setDescription('Create a new Drupal application')
+            ->addArgument('name', InputArgument::OPTIONAL)
+            ->addOption('dev', null, InputOption::VALUE_NONE, 'Installs the latest "development" release')
+            ->addOption('force', 'f', InputOption::VALUE_NONE, 'Forces install even if the directory already exists');
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if (! extension_loaded('zip')) {
+            throw new RuntimeException('The Zip PHP extension is not installed. Please install it and try again.');
+        }
+
+        $directory = ($input->getArgument('name')) ? getcwd().'/'.$input->getArgument('name') : getcwd();
+
+        if (! $input->getOption('force')) {
+            $this->verifyApplicationDoesntExist($directory);
+        }
+
+        $output->writeln('<info>Crafting application...</info>');
+
+        $this->download($zipFile = $this->makeFilename())
+             ->extract($zipFile, $directory)
+             ->cleanUp($zipFile);
+
+        $composer = $this->findComposer();
+
+        $commands = [
+            'mv ' . $directory . '/drupal-project-8.x/* .',
+            'rm -rf ' . $directory . '/drupal-project-8.x',
+            $composer.' run-script pre-install-cmd',
+            $composer.' run-script pre-update-cmd',
+            $composer.' install --no-interaction',
+            $composer.' run-script post-install-cmd',
+            $composer.' run-script post-update-cmd',
+        ];
+
+        $process = new Process(implode(' && ', $commands), $directory, null, null, null);
+
+        if ('\\' !== DIRECTORY_SEPARATOR && file_exists('/dev/tty') && is_readable('/dev/tty')) {
+            $process->setTty(true);
+        }
+
+        $process->run(function ($type, $line) use ($output) {
+            $output->write($line);
+        });
+
+        $output->writeln('<comment>Application ready! Build something amazing.</comment>');
+    }
+
+    /**
+     * Verify that the application does not already exist.
+     *
+     * @param  string  $directory
+     * @return void
+     */
+    protected function verifyApplicationDoesntExist($directory)
+    {
+        if ((is_dir($directory) || is_file($directory)) && $directory != getcwd()) {
+            throw new RuntimeException('Application already exists!');
+        }
+    }
+
+    /**
+     * Generate a random temporary filename.
+     *
+     * @return string
+     */
+    protected function makeFilename()
+    {
+        return getcwd().'/drupal_'.md5(time().uniqid()).'.zip';
+    }
+
+    /**
+     * Download the temporary Zip to the given file.
+     *
+     * @param  string  $zipFile
+     * @return $this
+     */
+    protected function download($zipFile)
+    {
+        $url = 'https://github.com/drupal-composer/drupal-project/archive/8.x.zip';
+        $response = (new Client)->get($url);
+
+        file_put_contents($zipFile, $response->getBody());
+
+        return $this;
+    }
+
+    /**
+     * Extract the Zip file into the given directory.
+     *
+     * @param  string  $zipFile
+     * @param  string  $directory
+     * @return $this
+     */
+    protected function extract($zipFile, $directory)
+    {
+        $archive = new ZipArchive;
+
+        $archive->open($zipFile);
+
+        $archive->extractTo($directory);
+
+        $archive->close();
+
+        return $this;
+    }
+
+    /**
+     * Clean-up the Zip file.
+     *
+     * @param  string  $zipFile
+     * @return $this
+     */
+    protected function cleanUp($zipFile)
+    {
+        @chmod($zipFile, 0777);
+
+        @unlink($zipFile);
+
+        return $this;
+    }
+
+    /**
+     * Get the composer command for the environment.
+     *
+     * @return string
+     */
+    protected function findComposer()
+    {
+        $composerPath = getcwd().'/composer.phar';
+
+        if (file_exists($composerPath)) {
+            return '"'.PHP_BINARY.'" '.$composerPath;
+        }
+
+        return 'composer';
+    }
+}
